6e245bf76ad10c942cfd187855cba3fbaec8817a
EDaley-FlemCollege <eric.daley@flemingcollege.ca>
2023-01-24 13:20:12 -0500

    Revert "Lab 1 Part 3"
    This reverts commit 4d727d2fda3b754863fee2833b796f4141046107.

--- Script1.py
+++ Script1.py
@@ -1,3 +1,4 @@
 #Print Lines from Zen of Python
-print('''Explicit is better than implicit.
+print('''Beautiful is better than ugly.
+Explicit is better than implicit.
 Simple is better than complex.''')
--- Script2.py
+++ Script2.py
@@ -1,4 +1,3 @@
-#Print Lines from Zen of Python
 print('''Complex is better than complicated.
 Flat is better than nested.
 Sparse is better than dense.''')
